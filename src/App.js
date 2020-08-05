8e8d0a90ee524f5e88e6d7c3519dd02901d0f95e
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-08-05 15:52:22 +0300

    Audio timer implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef, createRef } from "react";
 
-function App() {
+let timer;
+
+export default function App() {
+  const audioElement = useRef(null);
+
+
+  const [playing, setPlaying] = useState(false);
+  const [mood, setMood] = useState('sunny');
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  useEffect(() => {
+    if (audioElement.current !== null) {
+      if (playing) {
+        audioElement.current.play();
+      } else {
+        audioElement.current.pause();
+      }
+    }
+  }, [playing, mood]);
+
+  const setTimer = minutes => () => {
+    setSecondsLeft(minutes * 60 * 1000);
+  };
+
+  const mm = Math.floor(secondsLeft / 1000 / 60);
+  const ss = (secondsLeft / 1000) % 60;
+
+  useEffect(() => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      if (playing) {
+        console.log("plaing");
+
+        if (secondsLeft - 1000 >= 0) {
+          setSecondsLeft(secondsLeft - 1000);
+        } else {
+          setPlaying(false)
+          console.log("not playing");
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [playing, secondsLeft]);
+
+  const toggle = () => {
+    setPlaying(!playing);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={setTimer(1)}>1 minute</button>
+      <br />
+      <button onClick={setTimer(5)}>5 minutes</button>
+      <br />
+      <button onClick={setTimer(10)}>10 minutes</button>
+      <br />
+      <button onClick={setTimer(15)}>15 minutes</button>
+      <br />
+
+      <button onClick={() => setMood('sunny')}>Sunny</button>
+      <br />
+      <button onClick={() => setMood('cloudy')}>Cloudy</button>
+      <br />
+
+      <button onClick={toggle}>{playing ? 'Stop' : 'Start'}</button>
+      <br />
+
+      <div>
+        {mm}:{ss}
+      </div>
+
+      <audio ref={audioElement} key={mood}>
+        {/* <source src="melody.ogg" type="audio/ogg" /> */}
+        <source src={`${mood}.mp3`} type="audio/mpeg" />
+      </audio>
     </div>
   );
 }
-
-export default App;
